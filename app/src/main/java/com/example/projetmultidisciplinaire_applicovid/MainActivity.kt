0ee27defe2a4dd2f77acac723a7fbdf04737e1b9
Flairsus <flairsus@gmail.com>
2020-05-26 10:06:19 +0200

    I love you Kolomenek

--- app/src/main/java/com/example/projetmultidisciplinaire_applicovid/MainActivity.kt
+++ app/src/main/java/com/example/projetmultidisciplinaire_applicovid/MainActivity.kt
@@ -8,5 +8,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //I love eating my poop
     }
 }
