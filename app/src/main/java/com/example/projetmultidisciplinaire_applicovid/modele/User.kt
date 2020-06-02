e3500f862e425ea43516546ae0318552f1ec73d3
kolomenek <kolomenek@gmail.com>
2020-06-02 14:43:29 +0200

    Kamil Kt

--- app/src/main/java/com/example/projetmultidisciplinaire_applicovid/modele/User.kt
+++ app/src/main/java/com/example/projetmultidisciplinaire_applicovid/modele/User.kt
@@ -1,5 +1,15 @@
 package com.example.projetmultidisciplinaire_applicovid.modele
 
-class User {
+import java.time.LocalDate
 
+class User(
+    var usrLastName: String,
+    var usrFirstName: String,
+    var usrBirth: LocalDate,
+    var usrZipCode: Int,
+    var usrNumber: Int,
+    var usrStreet: String,
+    var usrCity: String
+) {
+    var usrAddress: String = "$usrNumber $usrStreet $usrZipCode $usrCity"
 }
