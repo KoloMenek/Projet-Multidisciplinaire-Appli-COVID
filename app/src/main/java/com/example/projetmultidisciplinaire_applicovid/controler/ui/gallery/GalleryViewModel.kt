800b028277a6e9e0704413e65fcd4bb7548d4f0a
flairsus <flairsus@gmail.com>
2020-06-14 16:13:33 +0200

    Menu

--- app/src/main/java/com/example/projetmultidisciplinaire_applicovid/controler/ui/gallery/GalleryViewModel.kt
+++ app/src/main/java/com/example/projetmultidisciplinaire_applicovid/controler/ui/gallery/GalleryViewModel.kt
@@ -8,7 +8,20 @@ import androidx.lifecycle.ViewModel
 class GalleryViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is gallery Fragment"
+        value = "\n\n- Éviter les contacts proches\n\n- Se laver fréquemment les mains\n\n- Éviter de se toucher les yeux, le nez et la bouche." +
+                "\n\n- Respecter les règles d’hygiène respiratoire \n\n- Tenez-vous informé et suivez les conseils de votre médecin\n\n\n QUESTIONS : \n\n\n" +
+                "Q : Faut-il éviter de se serrer la main à cause du nouveau coronavirus ?\n" +
+                "\n" +
+                "R : Oui. On peut contracter les virus respiratoires en serrant la main à quelqu’un puis en se touchant les yeux, le nez ou la bouche. Saluez d’un signe de la main ou de la tête, ou encore en vous inclinant.\n" +
+                "\n" +
+                "Q : Comment saluer une personne pour éviter d’attraper le nouveau coronavirus ?\n" +
+                "\n" +
+                "R : Le moyen le plus sûr de se saluer pour prévenir la COVID-19 est d’éviter les contacts physiques. On peut saluer d’un signe de la main ou de la tête, ou encore en s’inclinant.\n" +
+                "\n" +
+                "Q : Le port de gants en caoutchouc dans les lieux publics permet-il d’éviter l’infection par le nouveau coronavirus ?\n" +
+                "\n" +
+                "R : Non. Le fait de se laver les mains régulièrement protège mieux contre la COVID-19 que le port de gants en caoutchouc. " +
+                "Le virus peut se trouver sur les gants et il y a un risque de contamination si vous vous touchez le visage avec les gants.\n\n"
     }
     val text: LiveData<String> = _text
 }
--- app/src/main/java/com/example/projetmultidisciplinaire_applicovid/controler/ui/slideshow/SlideshowViewModel.kt
+++ app/src/main/java/com/example/projetmultidisciplinaire_applicovid/controler/ui/slideshow/SlideshowViewModel.kt
@@ -7,7 +7,35 @@ import androidx.lifecycle.ViewModel
 class SlideshowViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is slideshow Fragment"
+        value = "\n\nAttention à l’automédication \n " +
+                "CoVid-19 : quels sont les symptômes les plus fréquents ?\n\n" +
+                "Fièvre : 88% ; \n" +
+                "Toux sèche : 68% ; \n" +
+                "Fatigue: 38% ;\n" +
+                "Expectorations ou flegme épais des poumons : 33% ;\n" +
+                "Essoufflement : 19% ; \n" +
+                "Douleurs osseuses ou articulaires : 15% ;\n" +
+                "Maux de gorge : 14% ;\n" +
+                "Maux de tête : 14% ;\n" +
+                "Frissons : 11% ;\n" +
+                "Nausées ou vomissements : 5% ;\n" +
+                "Nez bouché : 5% ;\n" +
+                "Diarrhée : 4% ;\n" +
+                "Toux de sang : 1% ;\n" +
+                "Yeux gonflés : 1%.\n\n\n " +
+                "Comment agir en cas de suspicion d’infection ?\n\nEn cas de suspicion d’infection, le patient est invité à rester chez lui et à appeler son médecin traitant " +
+                "s’il ne présente que des symptômes légers, c’est-à-dire toux et fièvre. La téléconsultation est également possible et même à " +
+                "privilégier : un décret a été publié le 10 mars afin d’ “assouplir les conditions de réalisation de la télémédecine”. " +
+                "Le but : faire des professionnels de santé \"les premiers acteurs de la gestion sanitaire\" " +
+                "de l’épidémie, et ainsi limiter les déplacements pour éviter de potentielles contaminations dans les salles d’attente des cabinets médicaux. " +
+                "\n\nQuels symptômes doivent conduire à appeler le Samu ?\n" +
+                "Les urgences seront également désengorgées ; seules les personnes présentant des difficultés respiratoires sont invitées à appeler le Samu (15) en vue d’un dépistage et d’une hospitalisation éventuels.\n" +
+                "\n" +
+                "Le docteur Jimmy Mohamed, médecin d'Europe 1, a donné à travers une vidéo quelques astuces permettant de savoir si la gêne respiratoire ressentie nécessite une prise en charge. La première consiste à mesurer sa fréquence respiratoire, c'est-à-dire le nombre d'inspirations réalisées en une minute. L'idéal étant que quelqu'un d'autre fasse la mesure afin de ne pas se perdre dans le comptage. Si l'on fait plus de 20 inspirations par minute, cela définit une respiration rapide (polypnée) et nécessite donc d'avoir \"un contact médical urgent pour évaluer la situation\". \n" +
+                "\n" +
+                "Deuxième astuce : calculer son score de Roth. Cela consiste à prendre une inspiration profonde, puis compter de 1 à 30 de façon claire mais rapide. Il convient ensuite de regarder jusqu'à combien on a réussi à compter et combien de temps on a mis pour compter. Si l'on a réussi à compter jusqu'à 10 au maximum ou si l'on a dû reprendre une inspiration avant 7 secondes, cela signifie que le taux d'oxygène est inférieur à 95% et nécessite donc d'appeler le 15.\n" +
+                "\n" +
+                "Le médecin précise qu'il ne s'agit pas de techniques fiables à 100% : elles peuvent toutefois donner une idée, orienter le patient sur ce qu'il doit faire."
     }
     val text: LiveData<String> = _text
 }
